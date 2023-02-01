d624d97d346d46b2dbad25fd3b16ceca472f1213
Zayon84 <dalmkvist@hotmail.com>
2023-02-01 15:03:28 +0100

    new db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const https = require('https')
+app.use(express.json())
 
 const hero = { heros: [{ "name": "Bruce Wayne", "superheroName": "Batman"},
 { "name": "Peter Parker", "superheroName": "Spiderman"}]}
@@ -15,6 +16,20 @@ const game = { games: [{ "name": "Fallout4", "genre": "RPG", "developer": "Bethe
 { "name": "They are billions", "genre": "Stratergy", "developer": "Numantian Games"},
 { "name": "Colony Survival", "genre":" Sim Colony", "developer": "Pipliz"},
 { "name": "Eco", "genre": "Simulation", "developer": "Strange Loop Games"}]}
+
+var Movies = [
+        { title:"Avatar", actors:[
+            {name:'Pelle', role:'Main'},
+            {name:'Kalle', role:'Sidekick'},
+            {name:'Ralle', role:'Protagonist'}
+        ] },
+        { title: "Ironman", actors:[
+            {name:'Olle', role:'Main'},
+            {name:'Pär', role:'Sidekick'},
+            {name:'Karl', role:'Protagonist'}
+        ]
+        }
+    ]
 
 app.get("/joke", (req, res) =>{
     https.get("https://api.chucknorris.io/jokes/random", (response)=>{
@@ -43,8 +58,40 @@ app.get("/games", (req, res) =>{
 app.get("/", (req, res) =>{
     // res.send(game)
     res.sendfile(__dirname + "/index.html")
+    // console.log(Movies)
+    // res.send("hej")
 })
 
+app.get("/movies", (req, res) =>{
+    res.send(Movies)
+})
+
+app.get("/movies/:moviename", (req, res) =>{
+    const movieName = req.params.moviename.toLowerCase()
+    const myMovie = Movies.find(({title})=> title.toLowerCase() === movieName)
+    if(myMovie){
+        res.send(myMovie)
+    }
+    else{
+        res.status(404).send()
+    }
+})
+
+app.post("/movies", (req, res)=>{
+    // console.log(req.body)
+    Movies.push(req.body)
+    res.send()
+})
+
+app.delete("/movies/:moviename", (req, res) =>{
+    console.log(req.params)
+    const movieName = req.params.moviename.toLowerCase()
+    const id = Movies.findIndex(({title})=> title.toLowerCase() === movieName)
+    // console.log(id)
+    Movies.splice(id,1)
+    res.send()
+
+})
 
 app.listen(PORT, () => {
     console.log("Listening to port " + PORT)
